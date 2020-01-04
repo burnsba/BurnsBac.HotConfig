83ca40d58e947225d472d6fde532b03a648fd9bc
Ben Burns <benjaminaburns@gmail.com>
2020-01-04 13:11:40 -0600

    initial commit

---
+++ BurnsBac.HotConfig/DataSource/IConfigDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurnsBac.HotConfig.DataSource
+{
+    /// <summary>
+    /// Interface for skin json settings dropdown source.
+    /// </summary>
+    public interface IConfigDataProvider
+    {
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+CSharp project, dotnet core standard 2.0 library.  
+
+Code to load and interact with settings from a standardized json file.
